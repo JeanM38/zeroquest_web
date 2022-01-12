78a4795b4997e7ccbb2269501ed7e1365e41e9ad
Jean MIONNET <jean.mionnet38@gmail.com>
2022-01-12 10:34:52 +0100

    improve CI - test 4

--- src/App.js
+++ src/App.js
@@ -163,6 +163,7 @@ export function App() {
           {decks.map((deck, index) => (
             <Deck mb key={deck.type} data-testid={"deck"}>
               <h1>{deck.title}</h1>
+              <h1>Hello world</h1>
               <DeckItems data-testid={"deckitem"}>
                 <Droppable key={deck.type} id={deck.type} data={deck.type} >
                     {renderPiece(deck.type)}
